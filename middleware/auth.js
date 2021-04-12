26a977ec12cbd4d8d6fd4f5bb349133f329ba6ad
Gift <gkenny896@gmail.com>
2021-04-12 23:58:01 +0100

    ...

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
     }
 
     try {
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        const decoded = jwt.verify(token, config.get('JWTSECRET'));
 
         req.user = decoded.user;
         next();
